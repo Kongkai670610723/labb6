4f09c3060123bde1374e4c1d1c679638dca4da54
Kong670610723 <Phurin0946122606@gmail.com>
2024-12-06 13:53:57 +0700

    complete

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # 261102 lab6 
 # ชื่อ-สกุล: ภูรินท์ อินทจักร์
 # รหัสนักศึกษา: 670610723
-git config --global user.email "you@example.com"
-  git config --global user.name "Your Name"
--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,19 @@
 using namespace std;
 
 int main(){
+    int N;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> N;
+
+    int sum = 0;
+    while (N%2=0){
+        cout << "#Even numbers = " << sum << ;
+        sum = sum + 1;
+    }
+    
+    while (N = 0){
+        cout << "#Odd numbers = ";
+    }
+
     return 0;
 }
